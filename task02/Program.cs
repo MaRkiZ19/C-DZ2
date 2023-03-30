458d4496148965d4cf988a4992130de90cacb6d0
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-30 23:36:50 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Read()
+{
+    System.Console.WriteLine("Ведите трёхзначное число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = Read();
+
+int ThirdNum(int number)
+{
+    while (number > 999) number = number/10;
+    number = number % 10;
+    return number;
+}
+
+System.Console.WriteLine();
+if (number<100) System.Console.WriteLine("Третья цифра отсутсвует");
+else System.Console.WriteLine($"Третье цифра = {ThirdNum(number)}");
+System.Console.WriteLine();
