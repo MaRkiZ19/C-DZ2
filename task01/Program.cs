99b0fc2b30c78b2369ef4ec630feb9e5210280bb
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-30 23:06:19 +0300

    add task01

---
+++ task01/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Read()
+{
+    System.Console.WriteLine("Ведите трёхзначное число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = Read();
+
+int Average(int number)
+{
+    number = number / 10;
+    int Average = number % 10;
+    return Average;
+}
+
+System.Console.WriteLine();
+if (number>999 || number<100) System.Console.WriteLine("Неверное число! Введите ТРЁХ значное число!");
+else System.Console.WriteLine($"Вторая цифра 3х значного числа =  {Average(number)}");
+System.Console.WriteLine();
